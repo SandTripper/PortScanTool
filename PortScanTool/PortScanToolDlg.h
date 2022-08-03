9e8d192e60e3348201c561e7f7f22e92437db523
Sand_Tripper <20202131034@m.scnu.edu.cn>
2022-08-03 20:44:17 +0800

    添加项目文件。

---
+++ PortScanTool/PortScanToolDlg.h
@@ -0,0 +1,43 @@
+﻿
+// PortScanToolDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CPortScanToolDlg 对话框
+class CPortScanToolDlg : public CDialogEx
+{
+// 构造
+public:
+	CPortScanToolDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_PORTSCANTOOL_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CIPAddressCtrl m_strIP;
+	CStatic m_default_port;
+	CProgressCtrl m_progress_scanner;
+	CListCtrl m_list_data;
+	CEdit m_single_port;
+	CEdit m_from_port;
+	CEdit m_to_port;
+	CEdit m_thread_number;
+};
---
+++ PortScanTool/resource.h
@@ -0,0 +1,35 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 PortScanTool.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_PORTSCANTOOL_DIALOG         102
+#define IDP_SOCKETS_INIT_FAILED         103
+#define IDR_MAINFRAME                   128
+#define IDC_IPADDRESS_IP                1000
+#define IDC_RADIO_SINGLE_PORT           1001
+#define IDC_RADIO_MUL_PORT              1002
+#define IDC_EDIT_SINGLE_PORT            1003
+#define IDC_EDIT_MUL_PORT_FROM          1004
+#define IDC_EDIT_MUL_PORT_TO            1005
+#define IDC_EDIT_NUMBER_OF_THREAD       1006
+#define IDC_BTN_START_SCAN              1007
+#define IDC_BTN_STOP_SCAN               1008
+#define IDC_BTN_EXIT                    1009
+#define IDC_PROGRESS_SCANNER            1011
+#define IDC_LIST_IMFORMATION            1012
+#define IDC_LIST_DATA                   1012
+#define IDC_STATIC_DEFAULT_PORT         1013
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1014
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
